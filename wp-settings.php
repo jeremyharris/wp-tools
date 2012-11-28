2b1d1e6bd233d6bd71867191f344da7de5183ad5
Jeremy Harris <jharris@rockharbor.org>
2012-11-28 09:41:49 -0800

    Fixed typo

--- wp-settings.php
+++ wp-settings.php
@@ -1,5 +1,5 @@
 <?php
 /**
- * For some unexplicable reason WP loads classes in their configuration file,
+ * For some inexplicable reason WP loads classes in their configuration file,
  * so this file is here to be loaded intead of the actual `wp-settings.php` file.
  */
